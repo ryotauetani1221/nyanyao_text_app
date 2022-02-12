e2cc2575106cf43603471c24068e8606a38e27fe
ryotauetani1221 <r.uetani33@gmail.com>
2022-02-12 09:41:02 +0000

    fix

--- src/index.js
+++ src/index.js
@@ -8,21 +8,3 @@ Alpine.start()
 // window.test = () => {
 //     console.log('test')
 //  }
-
-window.makeImage = () => {
-    // console.log(fileName.value);
-    fileNameValue = fileName.value ? fileName.value : 'no_name';
-    const nodes = document.querySelectorAll('#js-image');
-    nodes.forEach((node, index) => {
-        htmlToImage.toPng(node, { cacheBust: true, })
-            .then(function (dataUrl) {
-                const link = document.createElement('a')
-                link.download = `${fileNameValue}_${index + 1}.png`;
-                link.href = dataUrl
-                link.click()
-            })
-            .catch(function (error) {
-                console.error('oops, something went wrong!', error);
-            });
-    });
-}
